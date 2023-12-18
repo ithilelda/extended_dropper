47a6297a062f2928dfd02eeabdb95a3fa4b5f485
ithilelda <ithilelda@gmail.com>
2023-12-18 16:04:35 +0800

    since many mod weapon do respect the vanilla hierarchy, I can safely assume killing action for only swords.

--- src/main/java/top/ithilelda/mixin/DropperBlockMixin.java
+++ src/main/java/top/ithilelda/mixin/DropperBlockMixin.java
@@ -2,40 +2,39 @@ package top.ithilelda.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.DropperBlock;
 import net.minecraft.block.dispenser.DispenserBehavior;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolItem;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPointer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.world.event.GameEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import top.ithilelda.ExtendedDropper;
 import top.ithilelda.utils.DropperActions;
 
 @Mixin(DropperBlock.class)
 public class DropperBlockMixin {
 	@WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/block/dispenser/DispenserBehavior;dispense(Lnet/minecraft/util/math/BlockPointer;Lnet/minecraft/item/ItemStack;)Lnet/minecraft/item/ItemStack;"), method = "dispense")
 	private ItemStack dispenseWithToolUsage(DispenserBehavior instance, BlockPointer blockPointer, ItemStack itemStack, Operation<ItemStack> original) {
-		// we only perform actions if we have chosen a valid tool.
-		ServerWorld world = blockPointer.getWorld();
-		Direction direction = blockPointer.getBlockState().get(DispenserBlock.FACING);
-		BlockPos facingPos = blockPointer.getPos().offset(direction);
-		BlockState breakingBlockState = world.getBlockState(facingPos);
+		// we disable the vanilla behavior for tool items.
 		if (itemStack.getItem() instanceof ToolItem) {
-			// if there is a block in front and we have the suitable tool, we try to use the tool to break it.
-			if (!breakingBlockState.isAir() && itemStack.isSuitableFor(breakingBlockState)) {
+			ServerWorld world = blockPointer.getWorld();
+			Direction direction = blockPointer.getBlockState().get(DispenserBlock.FACING);
+			BlockPos facingPos = blockPointer.getPos().offset(direction);
+			BlockState breakingBlockState = world.getBlockState(facingPos);
+			// if there is a block in front and we have the suitable mining tool, we try to use the tool to break it.
+			if (!breakingBlockState.isAir() && itemStack.getItem() instanceof MiningToolItem && itemStack.isSuitableFor(breakingBlockState)) {
 				DropperActions.BreakBlockWithTool(world, breakingBlockState, facingPos, itemStack);
 			}
-			// If there is no block in front and we have a tool item, we will try to attack entities within that block.
-			else if (breakingBlockState.isAir()) {
-				DropperActions.AttackEntityWithTool(world, facingPos, itemStack);
+			// If we've got a weapon, we try to swing it.
+			else if (itemStack.getItem() instanceof SwordItem) {
+				DropperActions.AttackEntityWithSword(world, facingPos, itemStack);
 			}
 			// otherwise, we do nothing. (note that tool items will never be dropped by dropper now).
 			return itemStack;
--- src/main/java/top/ithilelda/utils/DropperActions.java
+++ src/main/java/top/ithilelda/utils/DropperActions.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolItem;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
@@ -17,32 +18,26 @@ import top.ithilelda.ExtendedDropper;
 import java.util.List;
 
 public class DropperActions {
-    public static boolean BreakBlockWithTool(ServerWorld world, BlockState blockState, BlockPos pos, ItemStack tool) {
+    public static void BreakBlockWithTool(ServerWorld world, BlockState blockState, BlockPos pos, ItemStack tool) {
         world.emitGameEvent(GameEvent.BLOCK_DESTROY, pos, GameEvent.Emitter.of(null, blockState));
         Block breakingBlock = blockState.getBlock();
-        boolean result = world.removeBlock(pos, false);
-        if (result) {
+        if (world.removeBlock(pos, false)) {
             breakingBlock.onBroken(world, pos, blockState);
             Block.dropStacks(blockState, world, pos, world.getBlockEntity(pos), null, tool);
         }
-        boolean toolBroken = tool.damage(1, world.random, null);
-        if (toolBroken) {
+        if (tool.damage(1, world.random, null)) {
             tool.setCount(0);
         }
-        return result;
     }
-    public static void AttackEntityWithTool(ServerWorld world, BlockPos pos, ItemStack tool) {
+    public static void AttackEntityWithSword(ServerWorld world, BlockPos pos, ItemStack sword) {
         List<Entity> entities = world.getOtherEntities(null, new Box(pos));
         for (Entity entity : entities) {
             if (entity instanceof LivingEntity target) {
-                ExtendedDropper.LOGGER.info(tool.toString());
-                float toolDamage = tool.isEmpty() ? 0.0f : 3.0f + ((ToolItem)tool.getItem()).getMaterial().getAttackDamage();
-                toolDamage += EnchantmentHelper.getAttackDamage(tool, target.getGroup());
-                target.damage(world.getDamageSources().generic(), toolDamage);
-                int amount = tool.getItem() instanceof MiningToolItem ? 2 : 1;
-                boolean toolBroken = tool.damage(amount, world.random, null);
-                if (toolBroken) {
-                    tool.setCount(0);
+                float swordDamage = sword.isEmpty() ? 0.0f : ((SwordItem)sword.getItem()).getAttackDamage();
+                swordDamage += EnchantmentHelper.getAttackDamage(sword, target.getGroup());
+                target.damage(world.getDamageSources().generic(), swordDamage);
+                if (sword.damage(1, world.random, null)) {
+                    sword.setCount(0);
                 }
             }
         }
